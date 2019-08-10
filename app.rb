278d52b3abc14275d293f1fd1fbeb6913958b49d
Jamal Springer <jamal.springer@outlook.comm>
2019-08-10 22:36:37 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,29 @@
+
+require 'sinatra/base'
+
+class Battle <Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
+
+  get '/attack'
+  @player_1_name = session[:player_1_name]
+  @player_2_name = session[:player_2_name]
+  erb :attack
+
+  run! if app_file == $0
+end
